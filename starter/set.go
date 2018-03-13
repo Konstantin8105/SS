1f11951f8d598385a5b679a72617d4de0db4e47f
Konstantin8105 <konstantin8105@gmail.com>
2018-03-13 21:23:28 +0300

    starter redesign

--- starter/set.go
+++ starter/set.go
@@ -2,5 +2,7 @@ package starter
 
 // Set change setting of starter name `starterName`
 func Set(starterName string) {
+	m.Lock()
+	defer m.Unlock()
 	isStarter(starterName)
 }
--- starter/test.go
+++ starter/test.go
@@ -2,5 +2,7 @@ package starter
 
 // Test run test of starter name `starterName`
 func Test(starterName string) {
+	m.Lock()
+	defer m.Unlock()
 	isStarter(starterName)
 }
